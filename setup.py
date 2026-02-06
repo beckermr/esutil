a8fc2149a18074f9aa61549e835fce92fbd3dfb5
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-02-06 04:24:57 -0600

    Add debug print for extension build details
    Add print statement for debugging extension build.

--- setup.py
+++ setup.py
@@ -139,6 +139,8 @@ class MyBuilder(build_ext):
                 for flag in lflags:
                     e.extra_link_args.append(flag)
 
+        print(e, e.sources, e.extra_compile_args, flush=True)
+
         # Now run the normal build function.
         build_ext.build_extensions(self)
 
